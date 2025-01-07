401467d8b0721972aa30392f14b37e4ff02fd83b
sdm695dev <sdm695dev@gmail.com>
2025-01-07 03:52:25 +0000

    veux: Bringup Project-Infinity
    Signed-off-by: sdm695dev <sdm695dev@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_veux.mk
+    $(LOCAL_DIR)/infinity_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_veux-user \
-    lineage_veux-userdebug \
-    lineage_veux-eng
+    infinity_veux-user \
+    infinity_veux-userdebug \
+    infinity_veux-eng
