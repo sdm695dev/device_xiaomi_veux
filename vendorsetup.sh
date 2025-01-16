ab7776e4b3e68f820e86ec02cb0ec8a671ecd56d
Amrito | アンブロシア <amrito@project-pixelstar.xyz>
2025-01-16 16:10:48 +0530

    veux: shallow clone everything and switch to bit bucket for vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ rm -rf hardware/qcom-caf/sm8350/audio/configs/holi && git clone https://github.c
 
 echo "Cloning vendor..."
 # Clone Vendor
-git clone https://gitea.com/amritokun/vendor_xiaomi_veux.git -b 15 vendor/xiaomi/veux
+git clone --depth=1 https://amritokun-admin@bitbucket.org/amritokun/vendor_xiaomi_veux.git -b 15 vendor/xiaomi/veux
 
 echo "Cloning hardware related stuff..."
 # Hardware
@@ -12,17 +12,17 @@ git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b line
 
 echo "Cloning Kernel"
 # Kernel
-git clone https://github.com/PaimonLab/kernel_xiaomi_veux.git kernel/xiaomi/veux
+git clone --depth=1 https://github.com/PaimonLab/kernel_xiaomi_veux.git kernel/xiaomi/veux
 
 echo "Cloning BCR..."
 # BCR
-git clone https://github.com/MrTopia/vendor_bcr vendor/bcr
+git clone --depth=1 https://github.com/MrTopia/vendor_bcr vendor/bcr
 
 echo cloning AGC camera
-git clone https://gitlab.com/takixamru/vendor_xiaomi_agc-camera.git vendor/xiaomi/AGC-camera 
+git clone --depth=1 https://gitlab.com/takixamru/vendor_xiaomi_agc-camera.git vendor/xiaomi/AGC-camera 
 
 echo "Cloning MIUI Camera..."
 # MIUI Camera
-git clone https://github.com/Amrito-Projects/vendor_xiaomi_miuicamera-veux.git vendor/xiaomi/miuicamera-veux
-git clone https://github.com/Amrito-Projects/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
+git clone --depth=1 https://github.com/Amrito-Projects/vendor_xiaomi_miuicamera-veux.git vendor/xiaomi/miuicamera-veux
+git clone --depth=1 https://github.com/Amrito-Projects/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
 echo "Completed, proceeding to lunch"
