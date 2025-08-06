4805d268f9d96fdbf6ddc9e203e3f3cc92aba3a1
sdm695dev <sdm695dev@gmail.com>
2025-08-06 05:33:22 +0000

    veux: Update vendorsetup.sh
    Signed-off-by: sdm695dev <sdm695dev@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,29 +1,28 @@
-echo "Cloning Audio Configs...."
-# Clone own audio config tree
-rm -rf hardware/qcom-caf/sm8350/audio/configs/holi && git clone https://github.com/Amrito-Projects/hardware_qcom-caf_sm8350_audio_configs_holi.git -b 14 hardware/qcom-caf/sm8350/audio/configs/holi --depth=1
+echo "Cloning hardware related stuff"
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
 
-echo "Cloning vendor..."
-# Clone Vendor
-git clone --depth=1 https://github.com/Amrito-Projects/vendor_xiaomi_veux-new.git -b 15 vendor/xiaomi/veux
-
-echo "Cloning hardware related stuff..."
-# Hardware
-git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.1 hardware/xiaomi
+echo "Cloning vendor"
+git clone https://github.com/sdm695dev/vendor_xiaomi_veux.git vendor/xiaomi/veux --depth=1
 
 echo "Cloning Kernel"
-# Kernel
-git clone --depth=1 https://github.com/PaimonLab/kernel_xiaomi_veux.git kernel/xiaomi/veux
+git clone https://github.com/sdm695dev/kernel_xiaomi_veux.git kernel/xiaomi/veux --depth=1
 
-echo "cloning AGC camera..."
-git clone --depth=1 https://gitlab.com/takixamru/vendor_xiaomi_agc-camera.git vendor/xiaomi/AGC-Camera 
+echo "Cloning AGC Camera"
+git clone https://gitlab.com/takixamru/vendor_xiaomi_agc-camera.git vendor/xiaomi/AGC-Camera --depth=1
 
-# MIUI Camera (Lecia)
-echo "Cloning MIUI Camera (Lecia)..."
-git clone https://github.com/Onelots-Devices-Playground/vendor_xiaomi_miuicamera-veux vendor/xiaomi/miuicamera-veux
+echo "Cloning MIUI Camera"
+git clone https://github.com/Onelots-Devices-Playground/vendor_xiaomi_miuicamera-veux vendor/xiaomi/miuicamera-veux --depth=1
+
+echo "Cloning BCR"
+git clone https://github.com/Chaitanyakm/vendor_bcr vendor/bcr --depth=1
 
 echo "Cloning Sony Dolby"
 git clone https://github.com/userariii/vendor_sony_dolby.git vendor/sony/dolby
 
 echo "Cloning ViPER4AndroidFX"
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+echo "Cloning Infinity-X Keys"
+git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys vendor/infinity-priv/keys
 echo "Completed, proceeding to lunch"
